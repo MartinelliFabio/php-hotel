4029ab1a7c47aca80d3aa6c0e77dae02be9133d7
MartinelliFabio <fabio.martinelli996@gmail.com>
2022-11-28 15:57:52 +0100

    Printed the first data without styling

--- index.php
+++ index.php
@@ -39,8 +39,8 @@
         ],
 
     ];
+ 
 
-?>
 ?>
 
 <!DOCTYPE html>
@@ -50,9 +50,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Hotel</title>
 </head>
 <body>
-    
+    <div class="container">
+      <div class='d-flex flex-row flex-wrap justify-content-between'>
+        <?php foreach($hotels as $hotel) { ?>
+            <div>
+                <p><?php echo $hotel['name']; ?></p>
+                <p><?php echo $hotel['description']; ?></p>
+                <p><?php echo $hotel['parking']; ?></p>
+                <p><?php echo $hotel['vote']; ?></p>
+                <p><?php echo $hotel['distance_to_center']; ?></p>
+            </div>
+        <?php
+        }
+        ?>
+      </div>
+    </div>
 </body>
 </html>
